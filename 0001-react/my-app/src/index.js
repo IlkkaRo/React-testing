7b0716b59ef9aaeeff76664a428df2abd26f21de
Roinisto Ilkka <s1800092@student.businesscollege.fi>
2018-10-02 14:20:50 +0300

    prettify
    make it more pretty

--- 0001-react/my-app/src/index.js
+++ 0001-react/my-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Counter from './Counter'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
 registerServiceWorker();
